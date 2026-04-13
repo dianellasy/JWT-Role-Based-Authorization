035e60e37b650c84d159308a07b9bff6cc50af2c
Dianella Sy <dianellabsy@csu.fullerton.edu>
2026-04-13 13:04:09 -0700

    Protect DELETE Endpoint in SecurityConfig.java

--- src/main/java/com/example/bookstore_mongodb/config/SecurityConfig.java
+++ src/main/java/com/example/bookstore_mongodb/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import com.example.bookstore_mongodb.filter.JwtFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -28,6 +29,7 @@ public class SecurityConfig {
                         .requestMatchers("/api/auth/**").permitAll()
                         .requestMatchers(org.springframework.http.HttpMethod.GET, "/api/books/**").permitAll()
                         .requestMatchers(org.springframework.http.HttpMethod.POST, "/api/books/**").hasRole("USER")
+                        .requestMatchers(org.springframework.http.HttpMethod.DELETE, "/api/books/**").hasRole("ADMIN")   // Only ADMIN users can delete books
                         .anyRequest().authenticated()
                 )
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
